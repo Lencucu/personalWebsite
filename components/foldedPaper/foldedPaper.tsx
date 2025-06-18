28be5d29ad61c99cdc3b4f44104791d6cf09d515
Lencucu He <lencucu@aliyun.com>
2025-06-18 13:16:57 +0800

    fix0.1

--- components/foldedPaper/foldedPaper.tsx
+++ components/foldedPaper/foldedPaper.tsx
@@ -3,7 +3,7 @@
 import { gsap } from 'gsap';
 import Link from 'next/link';
 
-export default function FoldedPaper(obj){
+export default function FoldedPaper(obj:object){
 	let item = [], index = 0, classSetting='';
 	Object.entries(obj).forEach(([key, value]) => {
 		if(key!='className'){
