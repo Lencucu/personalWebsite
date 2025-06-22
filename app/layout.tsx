9737c9f0ef14047ef45bc3d92153a5dc857b3884
Lencucu He <lencucu@aliyun.com>
2025-06-22 21:44:48 +0800

    fix0.3

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html>
       <body>
-        <main>
+        <main className={smileySans.className}>
           {children}
         </main>
       </body>
