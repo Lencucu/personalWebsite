f3f71296ea35c298d7566ded550768e98730a3be
Lencucu He <lencucu@aliyun.com>
2025-07-13 00:05:03 +0800

    fix0.1 | small fix related with fonts

---
+++ app/fonts.tsx
@@ -0,0 +1,11 @@
+import localFont from 'next/font/local';
+
+export const smileySans = localFont({
+  src: './../src/fonts/SmileySans-Oblique.ttf.woff2',
+  variable: '--font-smileySans',
+})
+
+export const titleBlack = localFont({
+  src: './../src/fonts/优设标题黑.ttf',
+  variable: '--font-优设标题黑',
+})
