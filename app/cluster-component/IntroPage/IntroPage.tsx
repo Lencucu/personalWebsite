b1deac2d4b8ec37b902a58fd475cb47e2fa1a604
Lencucu He <lencucu@aliyun.com>
2025-07-24 21:38:11 +0800

    修复了展开的滑动逻辑 | beta1.5目标：日志用数据库记录，地图增加国内源，可自添加图片以及地图marker

--- app/cluster-component/IntroPage/IntroPage.tsx
+++ app/cluster-component/IntroPage/IntroPage.tsx
@@ -30,37 +30,39 @@ export default function HomePage({
   const title1Ref = useRef<HTMLDivElement>(null);
   const title2Ref = useRef<HTMLDivElement>(null);
   const title3Ref = useRef<HTMLDivElement>(null);
-  const [ticked, setTicked] = useState(false);
-  const startXRef = useRef<number | null>(null);
+  const [inDragging, setInDragging] = useState(false);
+  const posX_start_Ref = useRef<number | null>(null);
   const title1TweenRef = useRef<GSAPTween | null>(null);
   const title2TweenRef = useRef<GSAPTween | null>(null);
   const title3TweenRef = useRef<GSAPTween | null>(null);
 
-  const initialEffectX = leftSideWidth_distn[0] + leftSideWidth_distn[1];
+  const effectX_default = leftSideWidth_distn[0] + leftSideWidth_distn[1];
   const [effectX, setEffectX] = useState<number | null>(null);
-  const effectXRef_start = useRef<number | null>(null);
+  const effectX_start_Ref = useRef<number | null>(null);
 
-  const [lastMoveLen, setLastMoveLen] = useState<number>(0);
+  const lastPos = useRef<number>(0);
+  const lastLastPos = useRef<number>(lastPos.current);
 
   function on_Down(e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>) {
-    if (!ticked) {
-      if (!containerRef.current) return;
-      const rect = containerRef.current.getBoundingClientRect();
-      setTicked(true);
-      startXRef.current = 'touches' in e
-        ? e.touches[0].clientX - rect.left
-        : e.clientX - rect.left;
-      effectXRef_start.current = effectX;
-    }
+    if(inDragging || !containerRef.current) return;
+    const rect = containerRef.current.getBoundingClientRect();
+    setInDragging(true);
+    posX_start_Ref.current = 'touches' in e
+      ? (e.touches[0].clientX - rect.left)
+      : (e.clientX - rect.left);
+    effectX_start_Ref.current! = effectX;
+    lastPos.current! = effectX;
+    lastLastPos.current! = lastPos.current!;
+    // 注意两个值的区别，第一个是记录点击时effect的实时进度值，第二、三个是记录滑动时的鼠标实时位置以及实时前一刻的位置
   }
 
   useEffect( () =>{
     if(shouleGoBack)
-      setEffectX(initialEffectX);
+      setEffectX(effectX_default);
   });
 
   useEffect( () => {
-    setEffectX(initialEffectX);
+    setEffectX(effectX_default);
     gsap.to("#main-content", {
       opacity: 1,
       duration: 1,
@@ -91,54 +93,54 @@ export default function HomePage({
   useEffect( () => {
     const rect = containerRef.current!.getBoundingClientRect();
     function on_Up(e: MouseEvent | TouchEvent) {
-      if(!ticked) return;
-      else setTicked(false);
-      const endX = (e instanceof TouchEvent)?
+      if(!inDragging) return;
+      else setInDragging(false);
+      const posX_end = (e instanceof TouchEvent)?
         (e.changedTouches[0].clientX - rect.left):
         (e.clientX - rect.left);
-      const effect = (endX-startXRef.current!)/rect.width;
-      if(lastMoveLen>0 && effect>0.1) setEffectX(1);
-      else setEffectX(initialEffectX);
+
+      const effect = (posX_end - posX_start_Ref.current!)/rect.width;
+      // 判断最后是否有微左滑，因为结尾微左滑一般意味着浏览者想撤销拉开的操作
+      // 就算没有微左滑，如果只移动一点点，也意味着不确定，此时也不执行拉开操作
+      if(lastPos.current!>lastLastPos.current! && effect>0.1) setEffectX(1);
+      else setEffectX(effectX_default);
     }
-
-    window.addEventListener('mouseup', on_Up);
-    window.addEventListener('touchend', on_Up);
-    return () => {
-      window.removeEventListener('mouseup', on_Up);
-      window.removeEventListener('touchend', on_Up);
-    };
-  }, [ticked]);
-
-  useEffect( () => {
-    const rect = containerRef.current!.getBoundingClientRect();
     function on_Move(e: MouseEvent | TouchEvent) {
-      if(!ticked) return;
-      const middleX = (e instanceof TouchEvent)?
+      if(!inDragging) return;
+      const posX_tmp = (e instanceof TouchEvent)?
         (e.touches[0].clientX - rect.left):
         (e.clientX - rect.left);
 
-      let middleEffectX = effectXRef_start.current! + (middleX-startXRef.current!)/rect.width;
-      // console.log(middleEffectX - effectXRef_start.current);
-      if(middleEffectX > 1) setEffectX(1);
-      else if(middleEffectX < 0) setEffectX(0);
-      else setEffectX(middleEffectX);
-      setLastMoveLen(middleEffectX - effectX!);
+      // 这里第一个加数代表点击时effect的值（这里并不是所期望的实时动画过程中effect的值，而是动画终点值，还待完善）
+      // 第二个加数是移动量
+      let effectX_tmp = effectX_start_Ref.current! + (posX_tmp-posX_start_Ref.current!)/rect.width;
+      setEffectX(
+        effectX_tmp>1? 1:
+        (effectX_tmp<0? 0:effectX_tmp)
+        );
+      // 备份前一刻位置以及记录实时位置
+      lastLastPos.current! = lastPos.current!;
+      lastPos.current! = effectX_tmp;
     }
 
     window.addEventListener('mousemove', on_Move);
     window.addEventListener('touchmove', on_Move);
+    window.addEventListener('mouseup', on_Up);
+    window.addEventListener('touchend', on_Up);
     return () => {
       window.removeEventListener('mousemove', on_Move);
       window.removeEventListener('touchmove', on_Move);
+      window.removeEventListener('mouseup', on_Up);
+      window.removeEventListener('touchend', on_Up);
     }
-  },[ticked, lastMoveLen]);
+  },[inDragging]);
 
   useEffect( () => {
-    let distn = effectX! - initialEffectX;
+    let distn = effectX! - effectX_default;
     if(distn > 0)
-      distn = distn/(1 - initialEffectX)*(1 - leftSideWidth_distn[0]);
+      distn = distn/(1 - effectX_default)*(1 - leftSideWidth_distn[0]);
     else
-      distn = distn/initialEffectX*leftSideWidth_distn[0];
+      distn = distn/effectX_default*leftSideWidth_distn[0];
     onSwipe?.(leftSideWidth_distn[0] + distn);
     gsap.to(title1TweenRef.current!,{progress: effectX!});
     gsap.to(title2TweenRef.current!,{progress: effectX!});
